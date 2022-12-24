1bc13b725cce90da6b48491c1043feec9be68f39
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2022-12-24 21:47:44 +0530

    first file

---
+++ Test.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+
+/*
+boolean-  true , false - nextBoolean()
+byte - nextByte()
+short - nextShort()
+int - nextInt()
+long - nextLong()
+float - nextFloat()
+double - nextDouble()
+char - nextChar() - invalid
+
+class - 
+String - next() , nextLine()
+
+
+name(string)
+age - (byte)
+address ( string)
+pincode (int)
+mobile no (long)
+marks ( float)
+spi (double)
+gender(char - M , F)
+voting ( boolean)
+rank ( short )
+
+
+
+ */
+
+ public class Test{
+
+    public static void main(String[] args) {
+        
+        // System.out.println("3>5 it s tue or false ?");
+        // boolean b;
+        Scanner sc = new Scanner(System.in);
+        // b=sc.nextBoolean();
+        // System.out.println("Ans is "+b);
+
+        System.out.println("Enter your name: ");
+        String str=sc.nextLine();
+        System.out.println("Your name is "+ str);
+
+        char ch;
+        System.out.println("Enter single character: ");
+        ch = sc.next().charAt(0);
+        System.out.println(ch);
+    }
+
+ }
